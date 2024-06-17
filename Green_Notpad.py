a03f3c508f84d8bbe694600b087a9bafe499b4ad
Zakarye Ridoine <168565323+0xRekin@users.noreply.github.com>
2024-06-17 23:25:16 +0100

    Add files via upload

---
+++ Green_Notpad.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter import filedialog
+
+root = Tk()
+root.geometry("600x600")
+root.title("Green Notpad (Created by Zakarye Ridoine)")
+root.config(bg="green4")
+root.resizable(False,False)
+
+def save_file():
+    open_file = filedialog.asksaveasfile(mode = "w", defaultextension = ".txt")
+    if open_file is None :
+        return
+    text = str(entry.get(1.0,END))
+    open_file.write(text)
+    open_file.close()
+
+def open_file():
+    file = filedialog.askopenfile(mode = "r", filetypes = [("Text Files","*.txt")])
+    if file is not None :
+        content = file.read()
+    entry.insert(INSERT,content)
+
+
+entry = Text(root,height = "33",width = "72",wrap = WORD)
+entry.place(x = 10,y = 10)
+
+button1 = Button(root,width = "26",height = "2",bg = "#fff",text = "Save your file",command = save_file).place(x = 100,y = 550)
+button2 = Button(root,width = "26",height = "2",bg = "#fff",text = "Open your file",command = open_file).place(x = 300,y = 550)
+
+root.mainloop()
---
+++ README.txt
@@ -0,0 +1,4 @@
+===========Description=============
+
+
+Green Notepad is a simple text editor application created using Python's Tkinter library. It provides basic functionality for creating, editing, and saving text files. The user interface features a green theme and includes options to save and open text files.
